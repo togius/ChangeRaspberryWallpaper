29e7ef3e71e88b961b315d114fd41416138b38e2
Tolga ŞAHİN <38344718+togius@users.noreply.github.com>
2020-10-04 18:37:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import os
 import random as rnd
-import subprocess
+from pathlib import Path
+
 def Arkaplan():
     liste = os.listdir("/usr/share/rpd-wallpaper")
     dosya = liste[rnd.randint(0,len(liste)-1)]
-    picture_path = os.getcwd() + "/resim/" + dosya
+    picture_path = str(Path(__file__).parent.absolute()) + "/resim/" + dosya
     # komut = 'lxterminal -e bash -c "pcmanfm --set-wallpaper {0}; sleep 3;exec bash"'.format(picture_path)
     komut = 'lxterminal -e bash -c "pcmanfm --set-wallpaper {0};"'.format(picture_path)
     os.system(komut)
-    print("Arkaplan değiştirildi.")
+    print("Arkaplan değiştirildi.", picture_path)
 
 if __name__ == "__main__":
     Arkaplan()
