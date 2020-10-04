396de5ad3927f4ded95441d9b64946b53f97194a
Tolga ŞAHİN <38344718+togius@users.noreply.github.com>
2020-10-04 18:39:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import random as rnd
 from pathlib import Path
 
 def Arkaplan():
-    liste = os.listdir("/usr/share/rpd-wallpaper")
+    liste = os.listdir("resim")
     dosya = liste[rnd.randint(0,len(liste)-1)]
     picture_path = str(Path(__file__).parent.absolute()) + "/resim/" + dosya
     # komut = 'lxterminal -e bash -c "pcmanfm --set-wallpaper {0}; sleep 3;exec bash"'.format(picture_path)
