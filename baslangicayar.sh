0f807810f85b79b2a94c003c2e0fb4e8aaa425a8
Tolga ŞAHİN <38344718+togius@users.noreply.github.com>
2020-11-14 09:34:45 +0300

    Update baslangicayar.sh

--- baslangicayar.sh
+++ baslangicayar.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-#crontab -l | { cat; echo "* * * * * $DIR/tahtakilit.sh"; echo "@reboot $DIR/tahtakilit.sh"; } | crontab -
 YOL=$HOME/.config/autostart/bgdegistir.desktop
 echo "[Desktop Entry]" > $YOL
 echo "Encoding=UTF-8" >> $YOL
